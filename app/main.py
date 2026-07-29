9bdce48f40b113a1b618534d9ce4fbf81ff1df3d
zhavira nindya <zhaviranindya@gmail.com>
2026-07-29 08:51:19 +0700

    [feat] [tickets] update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 from app.routes.health_route import router as health_router
 from app.routes.tickets_route import router as tickets_router
 
-app = FastAPI(title="Backend FastAPI Template")
+app = FastAPI(title="Backend FastAPI Mini Project")
 app.include_router(health_router)
 app.include_router(tickets_router)
